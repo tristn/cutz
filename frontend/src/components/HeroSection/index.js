da976d48788d243329eda37e6696ff245f22aad8
tristn <tristn@users.noreply.github.com>
2022-06-30 13:43:51 -1000

    moving everything to frontend folder

---
+++ frontend/src/components/HeroSection/index.js
@@ -0,0 +1,55 @@
+import React, { useState } from 'react';
+import Video from '../../videos/video.mp4';
+import { Button } from '../ButtonElements';
+import {
+  HeroContainer,
+  HeroBg,
+  VideoBg,
+  HeroContent,
+  HeroH1,
+  HeroP,
+  HeroBtnWrapper,
+  ArrowForward,
+  ArrowRight
+} from './HeroElements';
+
+const HeroSection = () => {
+  const [hover, setHover] = useState(false);
+
+  const onHover = () => {
+    setHover(!hover);
+  };
+
+  return (
+    <HeroContainer id="home">
+      <HeroBg>
+        <VideoBg autoPlay loop muted src={Video} type="video/mp4" />
+      </HeroBg>
+      <HeroContent>
+        <HeroH1>Cheap Haircuts</HeroH1>
+        <HeroP>
+          Nappy hair? Need a confidence boost? Book an appointment today with
+          our professional barbers.
+        </HeroP>
+        <HeroBtnWrapper>
+          <Button
+            to="signup"
+            onMouseEnter={onHover}
+            onMouseLeave={onHover}
+            primary="true"
+            dark="true"
+            smooth={true}
+            duration={500}
+            spy={true}
+            exact="true"
+            offset={-80}
+          >
+            Book now {hover ? <ArrowForward /> : <ArrowRight />}
+          </Button>
+        </HeroBtnWrapper>
+      </HeroContent>
+    </HeroContainer>
+  );
+};
+
+export default HeroSection;
