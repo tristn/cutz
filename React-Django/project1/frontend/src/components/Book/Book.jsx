726a308be5e5b5c78cb1acd757d2170efea58839
tristn <tristn@users.noreply.github.com>
2022-06-30 18:30:36 -1000

    Working version

--- React-Django/project1/frontend/src/components/Book/Book.jsx
+++ React-Django/project1/frontend/src/components/Book/Book.jsx
@@ -1,14 +1,9 @@
 import Note from './Notes';
-// import BookHeader from './BookHeader';
-// import BookFooter from './BookFooter';
-// import './App.css';
 
 function Book() {
   return (
     <div className="Book">
-      {/* <BookHeader /> */}
       <Note />
-      {/* <BookFooter /> */}
     </div>
   );
 }
